dcb6b51bc8aff84173c1a5c2bf93c5048767875f
ProjectRedLow <project.redlow@gmail.com>
2017-05-21 21:28:19 +0200

    Added template

---
+++ config/settings.php
@@ -0,0 +1,3 @@
+<?php
+	$title = '--- | Project RedLow industries';
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+	include 'config/settings.php';
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><? echo $title;?></title>
+	</head>
+	<body>
+		<div class="content">
+			<?php
+				include 'includes/menu.php';
+			?>	
+			<div class="leftMenu">
+				<div class="stats">
+					<!--player statistics-->
+				</div>		
+			</div>
+			
+			<div class="playfield">
+				<!--connected via siteurl-->
+			</div>
+
+			<div class="rightMenu">
+				<!--crimes-->
+			</div>
+		</div>
+	</body>
+</html>
